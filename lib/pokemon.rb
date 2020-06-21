c1fd33b2f68d79e0b051610dd238d08cf2b9c968
aurel fontaine <ajfame11@gmail.com>
2020-06-21 20:27:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,20 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
+<<<<<<< HEAD
   require "pry"
   def initialize (hash)
     @id = hash[:id]
     @name = hash[:name]
     @type = hash[:type]
     @db = hash[:db]
+=======
+  
+  def initialize (hash)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+>>>>>>> 51d625908794c5dad48e8c012cb923598988c482
   end
   
   def self.save(name, type, database_connection)
@@ -15,6 +24,10 @@ class Pokemon
   def self.find(id, database_connection)
     found_pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
     Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: database_connection)
+<<<<<<< HEAD
+=======
+
+>>>>>>> 51d625908794c5dad48e8c012cb923598988c482
   end
 
 end
